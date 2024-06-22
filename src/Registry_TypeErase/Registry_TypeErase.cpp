a7d26f20572b695d5fb83af73c7e4733c5a620a3
juholee97 <156589691+juholee97@users.noreply.github.com>
2024-06-23 00:16:59 +0900

    feat : about erase type

---
+++ src/Registry_TypeErase/Registry_TypeErase.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <memory>
+class AnyFunction
+{
+private:
+    struct Base
+    {
+        virtual ~Base() {}
+        virtual void MyFunc( int a ) = 0;
+    };
+
+    template <typename Func>
+    struct Impl : public Base
+    {
+        Impl( Func f ) : m_f( f ) {}
+
+        void MyFunc( int a ) override
+        {
+            m_f( a );
+        }
+
+        Func m_f;
+    };
+
+    std::unique_ptr<Base> m_FuncPtr;
+
+public:
+    template <typename Func>
+    AnyFunction( Func f ) : m_FuncPtr( std::make_unique<Impl<Func>>( f ) ) {}
+
+    void operator()( int a )
+    {
+        m_FuncPtr->MyFunc( a );
+    }
+};
+
+void funcCall( int a )
+{
+    std::cout << "funcCall : " << a << std::endl;
+}
+
+int main()
+{
+    auto lamdaCall = [] ( int a ) { std::cout << "lamdaCall : " << a << std::endl; };
+
+    // AnyFunction 는 lamda, function 객체 둘 다 인자로 ok.
+    AnyFunction wrapLamda( lamdaCall );
+    AnyFunction wrapFunc( funcCall );
+
+    wrapLamda( 2 );
+    wrapFunc( 1 );
+
+    return 0;
+}

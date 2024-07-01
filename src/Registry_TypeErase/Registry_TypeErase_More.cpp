7ac48c676e2dd6a8ca4e66ea6d1276d9aae11ad4
juholee97 <156589691+juholee97@users.noreply.github.com>
2024-07-01 21:51:28 +0900

    feat : fix more detail example

--- src/Registry_TypeErase/Registry_TypeErase_More.cpp
+++ src/Registry_TypeErase/Registry_TypeErase_More.cpp
@@ -1,77 +1,65 @@
-#include <iostream>
 #include <vector>
+#include <cstdint>
+#include <iostream>
 #include <memory>
+class SensorVectorRegistry;
 
-enum class SamplingType
-{
-    eType_A,
-    eType_B,
-    eType_C,
-};
-
-struct SamplingBaseData
-{
-    const SamplingType type;
-    const int time;
-    SamplingBaseData(const SamplingType input_type, const int input_time)
-        : type ( input_type )
-        , time ( input_time )
-    {
-    }
-};
-
-template <typename SamplingTypes>
-struct SamplingData : public SamplingBaseData
-{
-    const SamplingTypes data;
-    SamplingData (const SamplingType input_type, const int input_time, const SamplingTypes & input_data)
-        : SamplingBaseData(input_type, input_time)
-        , data ( input_data )
-    {
-    }
-};
-
-class SamplingBuffer
+template<typename T>
+class SensorVector : public std::vector<T>
 {
 public:
-    SamplingBuffer( const SamplingType input_type )
-        : m_sampling_type(input_type)
-    {
-    }
-    void Push_Data( SamplingBaseData & data )
-    {
-        m_buffer.push_back(data);
-    }
-    std::size_t GetBufferSize()
-    {
-        return m_buffer.size();
-    }
-    bool isSameType( SamplingType type )
-    {
-        return (m_sampling_type == type);
-    }
-private:
-    std::vector<SamplingBaseData> m_buffer;
-    const SamplingType m_sampling_type;
+	SensorVector( SensorVectorRegistry* pRegister );
+	int32_t m_WriteCnt{ 0 };
 };
 
-class SamplingBufferRegister
+class Wrapper
 {
 public:
-    void Push_Buffer( std::unique_ptr<SamplingBuffer> buffer )
-    {
-        regist_buffers.push_back(buffer);
-    }
-    void Push_Data_To_Buffer ( SamplingBaseData & data )
-    {
-        for( const auto & buffer : regist_buffers )
-        {
-            if( true == buffer.get()->isSameType(data.type))
-            {
-                buffer.get()->Push_Data(data);
-            }
-        }
-    }
-private:
-    std::vector<std::unique_ptr<SamplingBuffer> > regist_buffers;
+	virtual ~Wrapper() {}
+	virtual void clear() = 0;
 };
+
+template<typename T>
+class VectorWrapper : public Wrapper
+{
+public:
+	VectorWrapper( SensorVector<T>* pVec ) : m_Vec( pVec ) {}
+
+	void clear() override
+	{
+		m_Vec->clear();
+		m_Vec->m_WriteCnt = 0;
+	}
+
+private:
+	SensorVector<T>* m_Vec;
+};
+
+class SensorVectorRegistry
+{
+public:
+	template<typename T>
+	void Regist( SensorVector<T>* pVec )
+	{
+		m_Registered.push_back(
+			std::make_unique<VectorWrapper<T>>( pVec )
+		);
+	}
+
+	void clear()
+	{
+		for ( auto& reg : m_Registered )
+		{
+			reg->clear();
+		}
+	}
+
+private:
+	std::vector<std::unique_ptr<Wrapper>> m_Registered;
+};
+
+template<typename T>
+SensorVector<T>::SensorVector( SensorVectorRegistry* pRegister )
+{
+	pRegister->Regist( this );
+}
